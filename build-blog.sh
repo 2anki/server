bc6e93bc9f58861a3bdeb5633dafc826bd0cd024
Alexander Alemayhu <alexander@alemayhu.com>
2020-09-22 15:36:22 +0200

    try again

--- build-blog.sh
+++ build-blog.sh
@@ -2,5 +2,4 @@
 
 [ ! -d "blog.2anki.net" ] && git clone https://github.com/alemayhu/blog.2anki.net
 
-cd blog.2anki.net && yarn install && yarn build && cd -
-cp -r blog.2anki.net/_site dist/blog
+cd blog.2anki.net && yarn install && yarn build && cp -r _site ../dist/blog
