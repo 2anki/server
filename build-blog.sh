5c994e811433b76b866cf2ba474d5aee9ae6af2c
Alexander Alemayhu <alexander@alemayhu.com>
2020-09-23 16:17:54 +0200

    remove blog and norwegian messages

--- build-blog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-[ ! -d "blog.2anki.net" ] && git clone https://github.com/alemayhu/blog.2anki.net
-
-cd blog.2anki.net && yarn install && yarn build && cp -r _site ../dist/blog
-cp /app/dist/android-chrome-192x192.png /app/dist/blog/img/favicon/favicon-192x192.png
