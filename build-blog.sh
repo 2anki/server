41fea7f22f7d6649c6f82359f7874a9835c1f3a9
Alexander Alemayhu <alexander@alemayhu.com>
2020-09-23 10:04:28 +0200

    build-blog: fix path again ;-)

--- build-blog.sh
+++ build-blog.sh
@@ -3,4 +3,4 @@
 [ ! -d "blog.2anki.net" ] && git clone https://github.com/alemayhu/blog.2anki.net
 
 cd blog.2anki.net && yarn install && yarn build && cp -r _site ../dist/blog
-cp dist/android-chrome-192x192.png dist/blog/img/favicon/favicon-192x192.png
+cp /app/dist/android-chrome-192x192.png /app/dist/blog/img/favicon/favicon-192x192.png
