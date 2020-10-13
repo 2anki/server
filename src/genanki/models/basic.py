ad57ec7df946d0a5694d0163cec26b6f86c477fc
Alexander Alemayhu <alexander@alemayhu.com>
2020-10-13 20:32:51 +0200

    add back changes from Alp

--- src/genanki/models/basic.py
+++ src/genanki/models/basic.py
@@ -2,18 +2,18 @@ from genanki import Model
 
 def basic_model(id, name, css):
     return Model(id, name,
-        fields=[
-            {"name": "AField"},
-            {"name": "BField"},
+       fields=[
+            {"name": "Front"},
+            {"name": "Back"},
             {"name": "MyMedia"},
         ],
         templates=[
             {
                 "name": "card1",
-                "qfmt": '<span class="front-text-pre">{{AField}}</span>',
-                "afmt": '<span class="front-text-post">{{AField}}</span>'
+                "qfmt": '<span class="front-text-pre">{{Front}}</span>',
+                "afmt": '<span class="front-text-post">{{Front}}</span>'
                 '<hr id="answer">'
-                '<span class="back-text">{{BField}}</span>',
+                '<span class="back-text">{{Back}}</span>',
             }
         ],
         css=css,
