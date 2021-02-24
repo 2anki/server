8d560e71f63a4b1c503c68e133a21ad4ac45f302
Alexander Alemayhu <alexander@alemayhu.com>
2021-02-24 22:27:01 +0100

    move route changes

---
+++ server/routes/checks.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {res.status(200).send('Notion to Anki')})
+
+exports.default = router
--- server/routes/version.js
+++ server/routes/version.js
@@ -9,8 +9,8 @@ const router = express.Router()
 
 const appInfo = JSON.parse(fs.readFileSync(resolvePath(__dirname, '../../package.json')).toString())
 
-router.get(function(req, res) {
-	res.statis(200).send(appInfo.version)	
+router.get('/', function(req, res) {
+	res.status(200).send(`Notion to Anki v${appInfo.version}`)	
 })
 
 exports.default = router
