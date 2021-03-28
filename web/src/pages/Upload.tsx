b55925bae774542e1f36e5662957acd4e25e4037
Alexander Alemayhu <alexander@alemayhu.com>
2021-03-28 19:12:11 +0200

    upload: add notice about it being the dev-server

--- web/src/pages/Upload.tsx
+++ web/src/pages/Upload.tsx
@@ -1,8 +1,27 @@
-const UploadPage = () => {
+const DevelopmentServerInfo = () => {
         return (
-                <>
+                <section className="hero is-small is-warning">
+                        <div className="hero-body has-text-centered">
+                                <p className="title">This is a development server</p>
+                                <p>
+                                        For the production version see
+                                        <a className="button" href="https://2anki.net">https://2anki.net</a>
+                                        <p>When reporting bugs, please make sure to share examples</p>
+                                </p>
+                        </div>
+                </section>
+        )
+}
+
+const UploadPage = () => {
+        const isDevelopment = window.location.host !== "2anki.net"
+
+
+        return (
+                <div style={{paddingTop: "4rem"}}>
+                        { isDevelopment ? <DevelopmentServerInfo /> : null}
                         <h2>Upload</h2>
-                </>
+                </div>
         )
 }
 
