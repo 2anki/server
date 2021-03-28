9237fc2c1ecefd6e25a6f07cfd733db2d0df1166
Alexander Alemayhu <alexander@alemayhu.com>
2021-03-28 19:29:04 +0200

    add the tabs in the upload page

--- web/src/pages/Upload.tsx
+++ web/src/pages/Upload.tsx
@@ -1,3 +1,5 @@
+import {Link, useLocation} from "react-router-dom"
+
 const DevelopmentServerInfo = () => {
         return (
                 <section className="hero is-small is-warning">
@@ -13,14 +15,29 @@ const DevelopmentServerInfo = () => {
         )
 }
 
+// A custom hook that builds on useLocation to parse
+// the query string for you. 
+// Reference: https://reactrouter.com/web/example/query-parameters
+function useQuery() {
+        return new URLSearchParams(useLocation().search);
+      }
+
 const UploadPage = () => {
         const isDevelopment = window.location.host !== "2anki.net"
-
+        const query = useQuery()
+        const view = query.get("view")
 
         return (
                 <div style={{paddingTop: "4rem"}}>
                         { isDevelopment ? <DevelopmentServerInfo /> : null}
-                        <h2>Upload</h2>
+                        <div className="tabs is-centered is-boxed">
+                                <ul>
+                                        <li  className={`${view === 'upload' ? 'is-active' : null}`}> <Link to="upload?view=upload">Upload</Link></li>
+                                        <li  className={`${view === 'template' ? 'is-active' : null}`} ><Link to="upload?view=template">Template</Link></li>
+                                        <li  className={`${view === 'deck-options' ? 'is-active' : null}`} ><Link to="upload?view=deck-options">Deck</Link></li>
+                                        <li  className={`${view === 'card-options' ? 'is-active' : null}`} ><Link to="upload?view=card-options">Card</Link></li>
+                                </ul>
+                        </div>
                 </div>
         )
 }
