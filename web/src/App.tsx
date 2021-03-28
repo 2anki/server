7f0455be3cabef2f0950dd98c9c3ae335e8a9fe3
Alexander Alemayhu <alexander@alemayhu.com>
2021-03-28 18:51:58 +0200

    Put the upload page first
    Turns out the default behaviour of the Router is to pick one that
    matches. '/' will always match in '/upload' ;-)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,46 +1,44 @@
-import { Route, Router, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import styled from "styled-components"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import UploadPage from "./pages/Upload"
-import HomePage from "./pages/Home"
+import styled from "styled-components";
 
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import UploadPage from "./pages/Upload";
+import HomePage from "./pages/Home";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const Layout = styled.div`
   display: flex;
-   flex-direction: column;
+  flex-direction: column;
   justify-content: space-between;
   height: 100vh;
-`
+`;
 
 const Container = styled.div`
   display: block;
   flex: 1 0 auto;
   padding: 4rem;
-`
+`;
 
 function App() {
-  const history = createBrowserHistory()
   return (
-      <Router history={history}>
-    <Layout>
-      <Header />
-      <Container>
-        <Switch>
-          <Route path="/">
-            <HomePage />
+    <Router>
+      <Layout>
+        <Header />
+        <Container>
+          <Switch>
+            <Route path="/upload">
+              <UploadPage />
             </Route>
-          <Route path="/upload">
-            <UploadPage />
+            <Route path="/">
+              <HomePage />
             </Route>
-        </Switch>
-        
-      </Container>
-      <Footer />
-    </Layout>
-      </Router>
+          </Switch>
+        </Container>
+        <Footer />
+      </Layout>
+    </Router>
   );
 }
 
