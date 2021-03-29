2a72659cb81f57765d214ded0d050d7b86e6d297
Alexander Alemayhu <alexander@alemayhu.com>
2021-03-29 20:17:27 +0200

    flesh out the deck options

--- web/src/components/DeckOptions.tsx
+++ web/src/components/DeckOptions.tsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  font-weight: bold;
+  color: #83c9f5;
+`;
+
 const DeckOptions = () => {
+  const key = "empty-description";
+  const deckNameKey = "deckName";
+  const [isEmpty, setIsEmpty] = useState(localStorage.getItem(key) === "true");
+  const [deckName, setDeckName] = useState(
+    localStorage.getItem(deckNameKey) || ""
+  );
+
   return (
     <div className="container">
       <div className="has-text-centered">
         <h2 className="title">Deck Options</h2>
+      </div>
+      <div className="box">
+        <div className="field">
+          <strong>Deck Description</strong>
+          <div className="field">
+            <input
+              type="checkbox"
+              checked={isEmpty}
+              onChange={() => {
+                const empty = !isEmpty;
+                localStorage.setItem(key, empty.toString());
+                setIsEmpty(empty);
+              }}
+            />
+            Empty description
+          </div>
+        </div>
+        <div className="field">
+          <strong>Deck Name</strong>
+          <div className="control">
+            <StyledInput
+              placeholder="Enter deck name (optional)"
+              value={deckName}
+              onChange={(event) => {
+                const newName = event.target.value;
+                if (newName !== deckName) {
+                  setDeckName(newName);
+                  localStorage.setItem(deckNameKey, deckName);
+                }
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
