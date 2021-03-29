a0a339bb5b16f787df2e470a19b8df49253087fc
Alexander Alemayhu <alexander@alemayhu.com>
2021-03-29 22:12:26 +0200

    add the template names

---
+++ web/src/components/TemplateName.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const TemplateName: React.FC<{
+  storageKey: string;
+  label: string;
+  placeholder: string;
+}> = ({ storageKey, label, placeholder }) => {
+  const [value, setValue] = useState(localStorage.getItem(storageKey) || "");
+
+  return (
+    <div className="field">
+      <label>{label}</label>
+      <div className="control">
+        <input
+          type="text"
+          className="input"
+          placeholder={placeholder}
+          value={value}
+          onChange={(event) => {
+            const newValue = event.target.value;
+            setValue(newValue);
+            localStorage.setItem(storageKey, newValue);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TemplateName;
--- web/src/components/TemplateOptions.tsx
+++ web/src/components/TemplateOptions.tsx
@@ -1,8 +1,42 @@
+import TemplateName from "./TemplateName";
+
 const TemplateOptions = () => {
   return (
     <div className="container">
       <div className="has-text-centered">
         <h2 className="title">Template Options</h2>
+      </div>
+
+      <div className="box">
+        <TemplateName
+          storageKey="basic_model_name"
+          placeholder="Defaults to n2a-basic"
+          label="Basic Template Name"
+        />
+        <TemplateName
+          storageKey="cloze_model_name"
+          placeholder="Defaults to n2a-cloze"
+          label="Cloze Template Name"
+        />
+        <TemplateName
+          storageKey="input_model_name"
+          placeholder="Defaults to n2a-input"
+          label="Input Template Name"
+        />
+
+        <hr />
+        <h2>Preview support is coming soon</h2>
+        <p>
+          Track the progress here
+          <a
+            style={{ paddingLeft: "0.2rem" }}
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/alemayhu/Notion-to-Anki/projects/2"
+          >
+            Card Type Template Manager
+          </a>
+        </p>
       </div>
     </div>
   );
