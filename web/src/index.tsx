1a319b7b3fe979ed15006706ee8d55716ccb0a4c
Alexander Alemayhu <alexander@alemayhu.com>
2021-04-05 11:46:22 +0200

    replace bulma in index.html with npm module trunx
    https://trunx.dev/

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bulma/css/bulma.css";
 
 ReactDOM.render(
   <React.StrictMode>
