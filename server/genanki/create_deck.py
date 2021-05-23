2anki/server

commit 28d9d2eb278efa42518f5774d41d40a9d00eb3fa

server: web: allow the user to override the templates

server/genanki/create_deck.py
@@ -71,7 +71,6 @@ if __name__ == "__main__":
         elif template == 'abhiyan':
             CSS = _read_template(template_dir, 'abhiyan.css', "", "")
             CLOZE_STYLE = _read_template(template_dir, "abhiyan_cloze_style.css", "", "")
-
             fmtClozeQ = _read_template(template_dir, "abhiyan_cloze_front.html", "", "")
             fmtClozeA = _read_template(template_dir, "abhiyan_cloze_back.html", "", "")
             fmtQ = _read_template(template_dir, "abhiyan_basic_front.html", "", "")
@@ -79,17 +78,43 @@ if __name__ == "__main__":
             fmtInputQ = _read_template(template_dir, "abhiyan_input_front.html", "", "")
             fmtInputA =_read_template(template_dir, "abhiyan_basic_back.html", "", "") # Note: reusing the basic back, essentially the same.
         # else notionstyle
+        CLOZE_STYLE = CLOZE_STYLE + "\n" + CSS
+
+        BASIC_STYLE = CSS
+        BASIC_FRONT = fmtQ
+        BASIC_BACK = fmtA
+        n2aBasic = mt.get("n2aBasic")
+        if n2aBasic:
+            BASIC_STYLE = n2aBasic["styling"]
+            BASIC_FRONT = n2aBasic["front"]
+            BASIC_BACK = n2aBasic["back"]
+
+        CLOZE_FRONT = fmtClozeQ
+        CLOZE_BACK = fmtClozeA
+        n2aCloze = mt.get("n2aCloze")
+        if n2aCloze:
+            CLOZE_STYLE = n2aCloze["styling"]
+            CLOZE_FRONT = n2aCloze["front"]
+            CLOZE_BACK = n2aCloze["back"]
+        
+        n2aInput = mt.get("n2aInput")
+        INPUT_FRONT = fmtInputQ
+        INPUT_BACK = fmtInputA
+        if n2aInput:
+            INPUT_STYLE = n2aInput["styling"]
+            INPUT_FRONT = n2aInput["front"]
+            INPUT_BACK = n2aInput["back"]
 
         for deck in data:
             cards = deck.get("cards", [])
             notes = []
             for card in cards:
                 fields = [card["name"], card["back"], ",".join(card["media"])]
-                model = basic_model(basic_model_id, basic_model_name, CSS, fmtQ, fmtA) 
+                model = basic_model(basic_model_id, basic_model_name, BASIC_STYLE, BASIC_FRONT, BASIC_BACK) 
                 if card.get('cloze', False) and "{{c" in card["name"] :
-                    model = cloze_model(cloze_model_id, cloze_model_name, CLOZE_STYLE + "\n" + CSS, fmtClozeQ, fmtClozeA)
+                    model = cloze_model(cloze_model_id, cloze_model_name, CLOZE_STYLE , CLOZE_FRONT, CLOZE_BACK)
                 elif card.get('enableInput', False) and card.get('answer', False):
-                    model = input_model(input_model_id, input_model_name, CSS, fmtInputQ, fmtInputA)
+                    model = input_model(input_model_id, input_model_name, INPUT_STYLE, INPUT_FRONT, INPUT_BACK)
                     fields = [
                         card["name"].replace("{{type:Input}}", ""),
                         card["back"],

server/parser/Settings.ts
@@ -1,3 +1,4 @@
+import TemplateFile from "../../web/src/model/TemplateFile";
 export default class Settings {
   readonly deckName: string | undefined;
   readonly useInput: boolean;
@@ -23,6 +24,9 @@ export default class Settings {
   readonly inputModelId: string;
   readonly template: string;
   readonly perserveNewLinesInSummary: boolean;
+  readonly n2aCloze: TemplateFile | undefined;
+  readonly n2aBasic: TemplateFile | undefined;
+  readonly n2aInput: TemplateFile | undefined;
 
   constructor(input: any) {
     this.deckName = input.deckName;
@@ -49,5 +53,27 @@ export default class Settings {
     this.basicModelId = input.basic_model_id;
     this.inputModelId = input.input_model_id;
     this.template = input.template;
+
+    if (input["n2a-basic"]) {
+      try {
+        this.n2aBasic = JSON.parse(input["n2a-basic"]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (input["n2a-cloze"]) {
+      try {
+        this.n2aCloze = JSON.parse(input["n2a-cloze"]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (input["n2a-input"]) {
+      try {
+        this.n2aInput = JSON.parse(input["n2a-input"]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }

web/src/components/modals/SettingsModal.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import { Modal, Button } from "trunx";
@@ -184,17 +185,9 @@ const SettingsModal: React.FC<{
 
               <hr />
               <h2>Preview support is coming soon</h2>
-              <p>
-                Track the progress here
-                <a
-                  style={{ paddingLeft: "0.2rem" }}
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://github.com/alemayhu/Notion-to-Anki/projects/2"
-                >
-                  Card Type Template Manager
-                </a>
-              </p>
+              <Button>
+                <Link to="/tm">Edit Template</Link>
+              </Button>
             </BlueTintedBox>
           </div>
         </section>

web/src/pages/Templates/TemplatePage.tsx
@@ -1,17 +1,8 @@
-import {
-  Section,
-  Title,
-  Subtitle,
-  Container,
-  Columns,
-  Column,
-  Button,
-} from "trunx";
+import { Section, Title, Container } from "trunx";
 import MonacoEditor from "react-monaco-editor";
 import { useCallback, useEffect, useState } from "react";
 import TemplateSelect from "../../components/TemplateSelect";
 import TemplateFile from "../../model/TemplateFile";
-import TemplatePreview from "./TemplatePreview";
 
 // Don't put in the render function, it gets recreated
 let files: TemplateFile[] = [];
@@ -25,6 +16,7 @@ async function fetchBaseType(name: string) {
 
 const TemplatePage = () => {
   const [code, setCode] = useState("");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [options, _setOptions] = useState({
     minimap: { enabled: false },
     colorDecorators: false,
@@ -33,7 +25,6 @@ const TemplatePage = () => {
   const [isBack, setIsBack] = useState(false);
   const [isStyling, setIsStyling] = useState(false);
   const [language, setLanguage] = useState("html");
-  const [isShowPreview, setShowPreview] = useState(false);
 
   const [currentCardType, setCurrentCardType] = useState(
     localStorage.getItem("current-card-type") || "n2a-basic"
@@ -65,6 +56,7 @@ const TemplatePage = () => {
   // Fetch the base presets from the server  or load from local storage (should only be called once)
   useEffect(() => {
     (async function () {
+      files = [];
       for (const key of ["n2a-basic", "n2a-input", "n2a-cloze"]) {
         const local = localStorage.getItem(key);
         if (local) {
@@ -130,12 +122,6 @@ const TemplatePage = () => {
         {ready && (
           <>
             <Title>Template Manager</Title>
-            <Subtitle hasTextDanger>This is a work in progress.</Subtitle>
-            <TemplatePreview
-              template={getCurrentCardType()}
-              isPreviewActive={isShowPreview}
-              onClickClose={() => setShowPreview(false)}
-            />
             <div className="field is-horizontal">
               <div className="field-label is-normal">
                 <label className="label">Template: </label>
@@ -190,7 +176,7 @@ const TemplatePage = () => {
               </label>
             </div>
             <MonacoEditor
-              width="60vh"
+              width="80vw"
               height="700px"
               language={language}
               theme="vs-dark"
@@ -201,9 +187,6 @@ const TemplatePage = () => {
             />
           </>
         )}
-        <Button isInfo onClick={() => setShowPreview(true)}>
-          Preview
-        </Button>
       </Container>
     </Section>
   );

commit 07d752a0fee9125740b0367b89d2a47b236a1757

V0.6.5 (#274)
* server: set default style for input cards

Fixes: 28d9d2eb278e (server: web: allow the user to override the templates, 2021-05-06)

* package: version 0.6.5

server/genanki/create_deck.py
@@ -100,6 +100,7 @@ if __name__ == "__main__":
         n2aInput = mt.get("n2aInput")
         INPUT_FRONT = fmtInputQ
         INPUT_BACK = fmtInputA
+        INPUT_STYLE = CSS
         if n2aInput:
             INPUT_STYLE = n2aInput["styling"]
             INPUT_FRONT = n2aInput["front"]