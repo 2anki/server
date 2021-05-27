db9b5c29b57b40fc83efe03f092d89519eff7e18
Alexander Alemayhu <alexander@alemayhu.com>
2021-05-27 11:01:17 +0200

    Misc fixes and follow up topics
    * Update README.md
    
    * web: handle drag / drop of files
    
    Closes: https://github.com/alemayhu/Notion-to-Anki/issues/285
    
    * web: add twitter share button
    
    * web: reword the download modal
    
    Also change button size on Ko-Fi.
    
    * web: add more emoji to download modal
    
    * github: revert back to npm for buil
    
    it's currently failing with weird error messages.
    
    * github: also use npm for integration tests
    
    Fixes build issues.
    
    * github: add run prefix
    
    Still breaking. sigh!

--- web/src/components/modals/DownloadModal.tsx
+++ web/src/components/modals/DownloadModal.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+const TweetURL =
+  "https://twitter.com/intent/tweet?text=Thank%20you%20%40AAlemayhu%20for%20saving%20me%20so%20much%20time%20with%20Notion%20to%20Anki%20%F0%9F%A4%A9%20Create%20flashcards%20faster%20on%20https%3A%2F%2F2anki.net%2Fupload";
 const DownloadModal: React.FC<{
   title: string;
   downloadLink: string;
@@ -38,11 +40,10 @@ const DownloadModal: React.FC<{
             <hr />
             <h3 className="title is-3"> Please Support Open Source 🙏🏾</h3>
             <p>
-              You can directly support the development and accelerate the
-              improvements! Pick your price ranging starting from{" "}
-              <strong>$1</strong>. This deck is brought to you by our amazing{" "}
-              <span> </span>
+              This web server is community funded by our amazing <span> </span>
               <a href="https://www.patreon.com/alemayhu">patrons</a> 🤩
+              <br /> You can directly support the development and accelerate the
+              improvements!
             </p>
             <div className="has-text-centered">
               <a
@@ -61,7 +62,7 @@ const DownloadModal: React.FC<{
               <p>
                 Not comfortable with reocurring expense? No worries, you can
                 send one-time contributions with{" "}
-                <a href="https://ko-fi.com/alemayhu">Ko-Fi</a>
+                <a href="https://ko-fi.com/alemayhu">Ko-Fi</a>.
               </p>
               <div className="has-text-centered">
                 <a
@@ -70,11 +71,25 @@ const DownloadModal: React.FC<{
                   target="_blank"
                 >
                   <img
-                    height="36"
-                    style={{ border: "0px", height: "36px" }}
+                    height="51"
+                    style={{ border: "0px", height: "51px" }}
                     src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
                     alt="Buy Me a Coffee at ko-fi.com"
                   />
+                </a>
+              </div>
+              <p>
+                You can also support the project by sharing it on Twitter. Help
+                others discover it too ✨
+              </p>
+              <div className="has-text-centered">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  className="is-info button"
+                  href={TweetURL}
+                >
+                  Share on Twitter
                 </a>
               </div>
             </div>
