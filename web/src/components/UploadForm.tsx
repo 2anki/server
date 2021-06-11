b38fb736b39be51e47ae4d0506e3722b341e03e1
Alexander Alemayhu <alexander@alemayhu.com>
2021-06-11 22:43:32 +0200

    web/upload: don't reset the file name
    The onchange won't retrigger when selecting the same file again.

--- web/src/components/UploadForm.tsx
+++ web/src/components/UploadForm.tsx
@@ -137,7 +137,6 @@ const UploadForm = () => {
               onClickClose={() => {
                 setDownloadLink("");
                 setDeckName("");
-                setSelectedFilename("");
               }}
             />
           )}
