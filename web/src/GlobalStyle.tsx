779138851b504cedc5f062cbc795622354ee0900
Alexander Alemayhu <alexander@alemayhu.com>
2021-07-10 22:32:18 +0200

    web: move global styles into styled
    Getting used to using styled-components.

--- web/src/GlobalStyle.tsx
+++ web/src/GlobalStyle.tsx
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -11,3 +14,6 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`;
+
+export default GlobalStyle;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import "bulma/css/bulma.css";
 
