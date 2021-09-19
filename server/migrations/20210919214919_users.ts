88a19217baee934cea2788a51b26e37ca24bed83
Alexander Alemayhu <alexander@alemayhu.com>
2021-09-19 23:53:10 +0200

    server/migrations: create basic user
    Will add fields for Notion and Ankiweb later.

---
+++ server/migrations/20210919214919_users.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.string("name", 255).notNullable();
+    table.string("email", 255).unique().notNullable();
+    table.string("password").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users");
+}
