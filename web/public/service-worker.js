5e924c021c4356d0582421f09f57c0d85e77f055
Alexander Alemayhu <alexander@alemayhu.com>
2021-09-24 20:29:07 +0200

    web: remove the service worker
    We are not really using it for much currently and it's causing errors
    for Firefox users.

--- web/public/service-worker.js
+++
@@ -1,5 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-
-workbox.routing.registerRoute(
-	({ request }) => request.destination === 'image', new workbox.strategies.CacheFirst()
-);
