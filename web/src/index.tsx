361f4aa69bafd46a0ebfdb8d379a101acb994feb
Alexander Alemayhu <alexander@alemayhu.com>
2021-09-24 20:35:08 +0200

    web: do not initialize Sentry on localhost
    Too many false positives get created otherwise.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -17,15 +17,17 @@ const LoadingScreen = () => {
   return <StyledLoader>Loading... </StyledLoader>;
 };
 
-Sentry.init({
-  dsn: "https://d7943c67af2f4e82b9eece16f1eb842b@o404766.ingest.sentry.io/5965051",
-  integrations: [new Integrations.BrowserTracing()],
+if (window.location.hostname !== "localhost") {
+  Sentry.init({
+    dsn: "https://d7943c67af2f4e82b9eece16f1eb842b@o404766.ingest.sentry.io/5965051",
+    integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
