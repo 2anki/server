440eda37453d767b925bf937e2d51c0e66acd47f
Alexander Alemayhu <alexander@alemayhu.com>
2021-10-02 07:48:25 +0200

    work in progress

--- web/src/pages/DashboardPage.js
+++ web/src/pages/DashboardPage.js
@@ -1,5 +1,5 @@
 import { Link, Route, Switch } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 const generalPages = ["Workspaces", "Templates", "Settings"];
@@ -63,7 +63,8 @@ const DashboardContent = () => {
 };
 
 const connectLink = () => {
-  const redirectUri = encodeURIComponent("https://2anki.net/connect-notion");
+  let host = window.location.host === "2anki.net" ? "https://2anki.net/connect-notion" : "https://dev.2anki.net/connect-notion";
+  const redirectUri = encodeURIComponent(host);
   const notionClientID = "384c361a-dc51-4960-abc1-b1e76665f8da";
   return `https://api.notion.com/v1/oauth/authorize?client_id=${notionClientID}&redirect_uri=${redirectUri}&response_type=code`;
 };
@@ -82,48 +83,10 @@ const DashboardPage = () => {
     updateMenUItem(item);
   };
 
-  const [accessToken, setToken] = useState("");
-  const [code, setCode] = useState("");
-
-  useEffect(() => {
-    let url = new URL(window.location.href);
-    const codeParam = url.searchParams.get("code");
-    if (codeParam) {
-      localStorage.setItem("code", codeParam);
-      localStorage.setItem("state", url.searchParams.get("state") || "");
-    }
-    setCode(localStorage.getItem("code") || "");
-    setToken(localStorage.getItem("access_token") || "");
-  }, []);
-
-  const getNotionKey = () => {
-    console.log("to be implemented");
-    fetch("/auth/create-key", {
-      headers: { code: code },
-    }).then((response) => {
-      response.json().then((payload) => {
-        console.log(payload);
-        for (const [key, value] of Object.entries(payload)) {
-          // @ts-ignore
-          localStorage.setItem(key, value);
-          if (key === "access_token") {
-            // @ts-ignore
-            setToken(value);
-          }
-        }
-      });
-    });
-  };
-
-
   return (
     <>
       {!connected && <div>
-        {!code && <a className="button is-link has-text-weight-semibold	" href={connectLink()}>Connect to Notion</a>}
-        {code && !accessToken && (
-          <button onClick={() => getNotionKey()}>Get Notion Key</button>
-        )}
-        {code && accessToken && <p>Nothing to see here yet, move along ;-)</p>}
+        <a className="button is-link has-text-weight-semibold	" href={connectLink()}>Connect to Notion</a>
       </div>}
       {connected &&
         <section className="columns is-fullheight">
