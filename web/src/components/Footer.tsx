cc82ea51de6d553c9c4da60d3ed13e85d70501e7
Ojas Inamdar <ojasinamdar101@gmail.com>
2021-10-04 01:07:11 +0530

    Added fortress logo to footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -109,6 +109,14 @@ const Footer = () => {
               />
             </a>
           </p>
+          <p>
+            <a href="https://fortress.no">
+              <img 
+                src="https://fortress.no/icons/logo.svg"  
+                style={{width:"60px"}} 
+                />
+            </a>
+          </p>
         </div>
       </div>
       <div className="content has-text-centered">
