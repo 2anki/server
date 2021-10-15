e1c1cb27c9e61574604e9814ed1edd94a887160b
Alexander Alemayhu <alexander@alemayhu.com>
2021-10-15 18:14:56 +0200

    adjust the icons

--- web/src/components/Dashboard/SearchObjectEntry.tsx
+++ web/src/components/Dashboard/SearchObjectEntry.tsx
@@ -5,9 +5,6 @@ import SettingsModal from "../modals/SettingsModal";
 const Entry = styled.div`
   display: flex;
   align-items: center;
-  :hover {
-    background: lightgray;
-  }
   grid-gap: 1.2rem;
   padding: 1rem;
   font-size: 2.4vw;
@@ -38,36 +35,43 @@ const SearchObjectEntry = ({ title, icon, url, id }) => {
   const [hover, setHover] = useState(false);
   return (
     <>
-      <Entry
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
+      <Entry>
         <ObjectMeta>
           <span>{icon}</span>
           <span>{title}</span>
         </ObjectMeta>
-        {hover && (
-          <ObjectActions>
-            <ObjectAction
-              url={url}
-              image="/icons/Notion_app_logo.png"
-              onClick={() => console.log("clicked Notion")}
-            />
-            <ObjectAction
-              url={`/notion/${id}/convert`}
-              image="/icons/Anki_app_logo.png"
-              onClick={() => console.log("clicked APKG")}
-            />
-            <ObjectAction
-              onClick={(event) => {
-                event.preventDefault();
-                setShowSettings(true);
-              }}
-              url={`/dashboard/${id}/settings`}
-              image="/icons/settings.svg"
-            />
-          </ObjectActions>
-        )}
+        <ObjectActions>
+          <ObjectAction
+            url={`/notion/${id}/convert`}
+            image="/icons/Anki_app_logo.png"
+            onClick={() => console.log("clicked APKG")}
+          />
+          {!hover && (
+            <div
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              <img width="32px" src="/icons/ellipsis.svg" alt="more" />
+            </div>
+          )}
+          {hover && (
+            <>
+              <ObjectAction
+                url={url}
+                image="/icons/Notion_app_logo.png"
+                onClick={() => console.log("clicked Notion")}
+              />
+              <ObjectAction
+                onClick={(event) => {
+                  event.preventDefault();
+                  setShowSettings(true);
+                }}
+                url={`/dashboard/${id}/settings`}
+                image="/icons/settings.svg"
+              />
+            </>
+          )}
+        </ObjectActions>
       </Entry>
       {/* TODO: Detect if this page is a official 2anki.net template duplicate then link directly to the page section with the settings */}
       <SettingsModal
