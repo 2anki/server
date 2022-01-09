93504179af6dae221466ca55fcc46bd7d89153fd
Alexander Alemayhu <alexander@alemayhu.com>
2022-01-09 19:43:37 +0100

    web: show the current limits

---
+++ web/src/components/BecomeAPatron.tsx
@@ -0,0 +1,11 @@
+const BecomeAPatron = () => (
+  <a rel="noreferrer" target="_blank" href="https://www.patreon.com/alemayhu">
+    <img
+      alt="Become a patreon button"
+      src="/become_a_patron_button.png"
+      loading="lazy"
+    />
+  </a>
+);
+
+export default BecomeAPatron;
