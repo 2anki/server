fa5a2da8e71abcde82b838f26bc1c685fc0a213f
Alexander Alemayhu <alexander@alemayhu.com>
2022-02-09 21:04:38 +0100

    web: move hero section into own file
    Getting too messy ._.

--- web/src/components/hero/HeroSection.tsx
+++ web/src/components/hero/HeroSection.tsx
@@ -1,7 +1,153 @@
+import { useState } from "react";
 import styled from "styled-components";
+import HeroSubtitle from "./HeroSubtitle";
+import HeroText from "./HeroText";
+import HeroTitle from "./HeroTitle";
+import PrimaryButton  from "../buttons/PrimaryButton";
 
-const HeroSection = styled.section`
+const StyledSection = styled.section`
   background: #e5e5e5;
 `;
+const StyledIframe = styled.iframe`
+  margin: 0 auto;
+  display: none;
+`;
+
+const VideoWrapper = styled.div`
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+  margin: 0 auto;
+`;
+
+const PlayIcon = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  :hover {
+    cursor: pointer;
+  }
+  object-fit: cover;
+`;
+
+const VideoPoster = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const PersonIllustration = styled.img`
+  z-index: 1;
+  position: absolute;
+`;
+
+const Illustration3 = styled(PersonIllustration)`
+  position: absolute;
+  width: 199.61px;
+  height: 261.34px;
+  bottom: 0;
+
+  @media (max-width: 1024px) {
+    width: 61.08px;
+    height: 79.97px;
+  }
+
+  bottom: -200px;
+  left: -100px;
+`;
+
+const Illustration2 = styled(PersonIllustration)`
+  bottom: -80px;
+  right: -87px;
+
+  @media (max-width: 1024px) {
+    width: 82.6px;
+    height: 89.62px;
+
+    bottom: 0;
+    right: 0;
+  }
+`;
+
+const Illustration1 = styled(PersonIllustration)`
+  top: -148px;
+  left: -100px;
+  width: 213px;
+  height: 200.69px;
+  @media (max-width: 1024px) {
+    width: 83px;
+    height: 78.32px;
+    top: 0;
+    left: 0;
+  }
+`;
+const HeroSection = () => {
+  const [play, setPlay] = useState(false);
+
+  return <StyledSection className="hero is-medium">
+        <div className="container">
+          <h1>
+            <HeroTitle>Everything you need to</HeroTitle>
+            <HeroSubtitle> learn fast</HeroSubtitle>
+          </h1>
+          <HeroText>
+            We are making it the easiest and fastest way to create beautiful
+            Anki flashcards for anyone anywhere around the world!
+          </HeroText>
+          <div className="is-flex is-justify-content-center	">
+            <PrimaryButton
+              destination="/upload"
+              text="Get Started"
+              onClickLink={() => {}}
+            />
+          </div>
+          <div style={{ position: "relative" }}>
+            <Illustration1
+              src="/illustrations/illustrations-1.svg"
+              alt="illustration of man"
+            />
+            <Illustration2
+              src="/illustrations/illustrations-2.svg"
+              alt="illustration of second man"
+            />
+            <Illustration3
+              src="/illustrations/illustrations-3.svg"
+              alt="illustration of third man"
+            />
+            <VideoWrapper>
+              {!play && (
+                <>
+                  <VideoPoster
+                    src="/video-poster.png"
+                    alt={"video"}
+                    onClick={() => setPlay(true)}
+                  />
+                  <PlayIcon
+                    className="is-hidden-mobile"
+                    src="/icons/play-icon.svg"
+                    width={50}
+                    height={50}
+                    alt={"play"}
+                    onClick={() => setPlay(true)}
+                  />
+                </>
+              )}
+              {play && (
+                <StyledIframe
+                  className="is-flex"
+                  width="800"
+                  height="418"
+                  src="https://www.youtube.com/embed/Ex3DuBvCo0Y?autoplay=1"
+                  title="Preview of 2anki.net - Beta Notion API Integration"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></StyledIframe>
+              )}
+            </VideoWrapper>
+          </div>
+        </div>
+      </StyledSection>
+}
 
 export default HeroSection;
+  
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 
-import PrimaryButton from "../components/buttons/PrimaryButton";
 import SupportSection from "../components/SupportSection";
-import HeroSubtitle from "../components/hero/HeroSubtitle";
-import HeroTitle from "../components/hero/HeroTitle";
-import HeroText from "../components/hero/HeroText";
 import HeroSection from "../components/hero/HeroSection";
-import { useState } from "react";
 
 const MascotImage = styled.img`
   height: 500px;
@@ -71,152 +66,14 @@ const Subscribe = () => {
   );
 };
 
-const StyledIframe = styled.iframe`
-  margin: 0 auto;
-  display: none;
-`;
-
-const VideoWrapper = styled.div`
-  max-width: 800px;
-  width: 100%;
-  margin: 0 auto;
-  position: relative;
-  margin: 0 auto;
-`;
-
-const PlayIcon = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  :hover {
-    cursor: pointer;
-  }
-  object-fit: cover;
-`;
-
-const VideoPoster = styled.img`
-  width: 100%;
-  object-fit: cover;
-`;
-
-const PersonIllustration = styled.img`
-  z-index: 1;
-  position: absolute;
-`;
-
-const Illustration3 = styled(PersonIllustration)`
-  position: absolute;
-  width: 199.61px;
-  height: 261.34px;
-  bottom: 0;
-
-  @media (max-width: 1024px) {
-    width: 61.08px;
-    height: 79.97px;
-  }
-
-  bottom: -200px;
-  left: -100px;
-`;
-
-const Illustration2 = styled(PersonIllustration)`
-  bottom: -80px;
-  right: -87px;
-
-  @media (max-width: 1024px) {
-    width: 82.6px;
-    height: 89.62px;
-
-    bottom: 0;
-    rigth: 0;
-  }
-`;
-
-const Illustration1 = styled(PersonIllustration)`
-  top: -148px;
-  left: -100px;
-  width: 213px;
-  height: 200.69px;
-  @media (max-width: 1024px) {
-    width: 83px;
-    height: 78.32px;
-    top: 0;
-    left: 0;
-  }
-`;
-
 const HomePage = () => {
   const index = Math.round(Math.random() * 4);
   const image = `mascot/Notion ${index + 1}.png`;
-  const [play, setPlay] = useState(false);
 
   return (
     <>
-      <HeroSection className="hero is-medium">
-        <div className="container">
-          <h1>
-            <HeroTitle>Everything you need to</HeroTitle>
-            <HeroSubtitle> learn fast</HeroSubtitle>
-          </h1>
-          <HeroText>
-            We are making it the easiest and fastest way to create beautiful
-            Anki flashcards for anyone anywhere around the world!
-          </HeroText>
-          <div className="is-flex is-justify-content-center	">
-            <PrimaryButton
-              destination="/upload"
-              text="Get Started"
-              onClickLink={() => {}}
-            />
-          </div>
-          <div style={{ position: "relative" }}>
-            <Illustration1
-              src="/illustrations/illustrations-1.svg"
-              alt="illustration of man"
-            />
-            <Illustration2
-              src="/illustrations/illustrations-2.svg"
-              alt="illustration of second man"
-            />
-            <Illustration3
-              src="/illustrations/illustrations-3.svg"
-              alt="illustration of third man"
-            />
-            <VideoWrapper>
-              {!play && (
-                <>
-                  <VideoPoster
-                    src="/video-poster.png"
-                    alt={"video"}
-                    onClick={() => setPlay(true)}
-                  />
-                  <PlayIcon
-                    className="is-hidden-mobile"
-                    src="/icons/play-icon.svg"
-                    width={50}
-                    height={50}
-                    alt={"play"}
-                    onClick={() => setPlay(true)}
-                  />
-                </>
-              )}
-              {play && (
-                <StyledIframe
-                  className="is-flex"
-                  width="800"
-                  height="418"
-                  src="https://www.youtube.com/embed/Ex3DuBvCo0Y?autoplay=1"
-                  title="Preview of 2anki.net - Beta Notion API Integration"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></StyledIframe>
-              )}
-            </VideoWrapper>
-          </div>
-        </div>
-      </HeroSection>
-      <div></div>
+      <HeroSection />
+            <div></div>
       <section  className="section">
         <Container className="container">
           <CtaGroup>
