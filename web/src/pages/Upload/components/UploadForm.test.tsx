4e792275c21e3138157b39d274bfc5be8a216318
Alexander Alemayhu <alexander@alemayhu.com>
2022-03-26 14:47:41 +0100

    style: fix code smells from Sonarcloud

--- web/src/pages/Upload/components/UploadForm.test.tsx
+++ web/src/pages/Upload/components/UploadForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import UploadForm from "./UploadForm";
@@ -6,13 +6,13 @@ import UploadForm from "./UploadForm";
 describe("UploadForm", () => {
   test("download button is light by default", () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => {}} errorMessage="" />
+      <UploadForm setErrorMessage={(error) => fail(error)} errorMessage="" />
     );
     expect(container.querySelector(".button.cta.is-light")).toBeInTheDocument();
   });
   test("no null classes", () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => {}} errorMessage="" />
+      <UploadForm setErrorMessage={(error) => fail(error)} errorMessage="" />
     );
     expect(container.querySelector(".null")).toBeNull();
   });
