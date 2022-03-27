3616cb594b319778047c6ed65af9e35caa6e79ad
Alexander Alemayhu <alexander@alemayhu.com>
2022-03-27 10:31:45 +0200

    fix: add padding to container

--- web/src/components/styled.tsx
+++ web/src/components/styled.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: block;
   flex: 1 0 auto;
+  padding: 32px 64px;
+  @media (max-width: 1024px) {
+    padding: 0;
+  }
 `;
 
 export const Main = styled.main`
