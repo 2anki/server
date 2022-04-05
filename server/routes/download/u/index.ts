9a9d42467cfd6bc1bb389f731d6bbae974eb5c20
Alexander Alemayhu <alexander@alemayhu.com>
2022-04-05 18:49:27 +0200

    refactor: try to fix sonarcloud errors

--- server/routes/download/u/index.ts
+++ server/routes/download/u/index.ts
@@ -7,16 +7,16 @@ import StorageHandler from "../../../lib/storage/StorageHandler";
 const router = express.Router();
 
 let storage = new StorageHandler();
-router.get("/u/:key", RequireAuthentication, async (req, res) => {
-  const key = req.params.key;
-  console.debug("download " + key);
+router.get("/u/:userKey", RequireAuthentication, async (req, res) => {
+  const key = req.params.userKey;
   if (!key) {
     return res.status(400).send();
   }
+
   let owner = res.locals.owner;
   try {
     const match = await DB("uploads")
-      .where({ key, owner })
+      .where({ key: key, owner: owner })
       .returning(["key"])
       .first();
     if (match) {
@@ -28,7 +28,7 @@ router.get("/u/:key", RequireAuthentication, async (req, res) => {
   } catch (error) {
     // @ts-ignore
     if (error && error.code === "NoSuchKey") {
-      await DB("uploads").del().where({ key, owner });
+      await DB("uploads").del().where({ key: key, owner: owner });
       return res.redirect("/uploads");
     } else {
       console.info("unknown error");
