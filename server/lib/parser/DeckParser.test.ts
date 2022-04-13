b7f93007f69165fceb5ac542ddd9ca1c9611ec93
Alexander Alemayhu <alexander@alemayhu.com>
2022-04-13 15:46:42 +0200

    refactor: move all DeckParser tests into one file

--- server/lib/parser/DeckParser.test.ts
+++ server/lib/parser/DeckParser.test.ts
@@ -16,7 +16,7 @@ test.skip("Toggle Headings", async () => {
   expect(deck.cards.length).toBe(1);
 });
 
-test.skip("Grouped cloze deletions", async () => {
+test("Grouped cloze deletions", async () => {
   const deck = await getDeck(
     "Grouped Cloze Deletions fbf856ad7911423dbef0bfd3e3c5ce5c 3.html",
     new Settings({ cherry: "false", cloze: "true" })
@@ -24,3 +24,63 @@ test.skip("Grouped cloze deletions", async () => {
   expect(deck.name).toBe("Grouped Cloze Deletions");
   expect(deck.cards.length).toBe(20);
 });
+
+test("Cloze Deletions", async () => {
+  const deck = await getDeck(
+    "Some Cloze Deletions 1a118169ada841a99a9aaccc7eaa6775.html",
+    new Settings({ cherry: "false" })
+  );
+  expect(deck.cards[0].back).toBe(
+    "<div class='toggle'>{{c2::Canberra}} was founded in {{c1::1913}}.</div>"
+  );
+  expect(deck.cards[1].back).toBe(
+    "<div class='toggle'>{{c1::Canberra::city}} was founded in {{c2::1913::year}}</div>"
+  );
+  expect(deck.cards[2].back).toBe(
+    "<div class='toggle'>{{c1::Canberra::city}} was founded in {{c2::1913}}</div>"
+  );
+  expect(deck.cards[3].back).toBe(
+    "<div class='toggle'>{{c1::This}} is a {{c2::cloze deletion}}</div>"
+  );
+  expect(deck.cards[4].back).toBe(
+    "<div class='toggle'>{{c2::Canberra}} was founded in {{c1::1913}}.</div>"
+  );
+  expect(deck.cards[5].back).toEqual(
+    "<div class='toggle'>{{c1::Canberra::city}} was founded in {{c2::1913::year}}</div>"
+  );
+});
+
+test("Colours", async () => {
+  const deck = await getDeck(
+    "Colours 0519bf7e86d84ee4ba710c1b7ff7438e.html",
+    new Settings({ cherry: "false" })
+  );
+  expect(deck.cards[0].back.includes("block-color")).toBe(true);
+});
+
+test.skip("HTML Regression Test", (t) => {
+  t.fail(
+    "please automate HTML regression check. Use this page https://www.notion.so/HTML-test-4aa53621a84a4660b69e9953f3938685."
+  );
+});
+
+test("Nested Toggles", async () => {
+  const deck = await getDeck(
+    "Nested Toggles.html",
+    new Settings({ cherry: "true" })
+  );
+  expect(deck.cards.length).toBe(12);
+});
+
+test("Global Tags", async () => {
+  const deck = await getDeck(
+    "Global Tag Support.html",
+    new Settings({ tags: "true", cherry: "false" })
+  );
+  // use toContain
+  expect(deck.cards[0].tags.includes("global")).toBe(true);
+});
+
+test.todo("Input Cards ");
+test.todo("Multiple File Uploads");
+test.todo("Test Basic Card");
