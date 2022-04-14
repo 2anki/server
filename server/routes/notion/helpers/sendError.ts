9316a5d27a57ef4e55132edebff67aef01e8c5d0
Alexander Alemayhu <alexander@alemayhu.com>
2022-04-14 12:48:37 +0200

    fix: ensure that user supplied notion endpoints get a response
    There were some cases where errors were not propagated correctly.

---
+++ server/routes/notion/helpers/sendError.ts
@@ -0,0 +1,12 @@
+import {Response} from "express";
+
+import { APIResponseError } from "@notionhq/client/build/src";
+
+export default function sendError(error: Error | APIResponseError | unknown, response: Response) {
+    if (error instanceof APIResponseError) {
+      return response.status(error.status).json({message: error.message}).send();
+    } else {
+      /* @ts-ignore */
+      return response.status(500).json({message: error?.message}).send();
+    }
+}
