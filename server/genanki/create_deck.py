cefcea10867a8e698c7f3f6cb625ee57a35ee238
Alexander Alemayhu <alexander@alemayhu.com>
2022-04-16 09:15:53 +0200

    fix: don't assume `notionId` is present

--- server/genanki/create_deck.py
+++ server/genanki/create_deck.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
                         ",".join(card["media"]),
                     ]
                 # Cards marked with -1 number means they are breaking compatability, treat them differently by using their respective Notion Id
-                if card["number"] == -1:
+                if card["number"] == -1 and "notionId" in card:
                     card["number"] = card["notionId"]
 
                 if mt.get("useNotionId") and "notionId" in card:
