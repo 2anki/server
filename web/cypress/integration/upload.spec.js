6a80328e5c3427751965b239368b069061704729
Alexander Alemayhu <alexander@alemayhu.com>
2022-05-08 16:42:57 +0200

    test: check settings modal does close

--- web/cypress/integration/upload.spec.js
+++ web/cypress/integration/upload.spec.js
@@ -1,9 +1,26 @@
-describe('Upload', () => {
-  it('user can open settings', () => {
+const openSettingsModal = () => {
     cy.visit('/upload');
     cy.findByRole('link', { name: /settings/i }).click();
+}
+
+const closeSettingsModal = () => {
+    cy.findByRole('button', {
+      name: /close/i
+    }).click()
+}
+
+describe('Upload', () => {
+  it('user can open settings', () => {
+    openSettingsModal();
     cy.findByRole('heading', {
       name: /card options/i,
     });
+  });
+  it('user can close settings', () => {
+    openSettingsModal();
+    closeSettingsModal();
+    cy.findByRole('heading', {
+      name: /card options/i,
+    }).should('not.exist');
   });
 });
