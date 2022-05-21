4341d4fb406199b2b4812fabdf8b032828eca119
Alexander Alemayhu <alexander@alemayhu.com>
2022-05-21 21:01:53 +0200

    style: get the linting working correctly

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,16 +4,19 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: "./tsconfig.json"
   },
   plugins: [
+    "import",
     '@typescript-eslint',
   ],
   rules: {
-  },
+    "react/jsx-filename-extension": "off"
+   }
 };
