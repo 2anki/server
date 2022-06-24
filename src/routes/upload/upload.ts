e770fb6e5cf055812db91aad14da8e218af1d67b
Alexander Alemayhu <alexander@alemayhu.com>
2022-06-24 19:20:56 +0200

    feat: remove file size upload limits for patreons

--- src/routes/upload/upload.ts
+++ src/routes/upload/upload.ts
@@ -4,9 +4,13 @@ import multerS3 from 'multer-s3';
 import { MAX_FIELD_SIZE, MAX_UPLOAD_SIZE } from '../../lib/misc/file';
 import StorageHandler from '../../lib/storage/StorageHandler';
 
-export default function upload(storage: StorageHandler) {
+export default function upload(storage: StorageHandler, isPatron: boolean) {
+  const limits = isPatron
+    ? {}
+    : { fileSize: MAX_UPLOAD_SIZE, fieldSize: MAX_FIELD_SIZE };
+
   return multer({
-    limits: { fileSize: MAX_UPLOAD_SIZE, fieldSize: MAX_FIELD_SIZE },
+    limits,
     storage: multerS3({
       s3: storage.s3,
       bucket: StorageHandler.DefaultBucketName(),
