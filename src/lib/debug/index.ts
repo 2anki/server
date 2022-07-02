090abec6cfb973cca4c3bbbdc9e551fee4941369
Alexander Alemayhu <alexander@alemayhu.com>
2022-07-02 19:46:54 +0200

    refactor(debug): check default bucket to determine if debug

--- src/lib/debug/index.ts
+++ src/lib/debug/index.ts
@@ -1,2 +1,6 @@
+/**
+ *  If the default bucket is not set, we can safely assume we are running in debug mode.
+ * @returns boolean
+ */
 export const IsDebug = () =>
-  process.env.SPACES_DEFAULT_BUCKET_NAME == 'dev.2anki.net';
+  process.env.SPACES_DEFAULT_BUCKET_NAME === undefined;
