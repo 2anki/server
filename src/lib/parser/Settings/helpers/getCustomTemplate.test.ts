6faf687984579d2cdaea16ec3702c143218050ac
Alexander Alemayhu <alexander@alemayhu.com>
2022-07-31 19:59:58 +0200

    test: add missing case for input template

--- src/lib/parser/Settings/helpers/getCustomTemplate.test.ts
+++ src/lib/parser/Settings/helpers/getCustomTemplate.test.ts
@@ -8,15 +8,25 @@ const BASIC_TEMPLATE_FILE = {
   styling: '',
   storageKey: 'n2a-basic',
 };
+const CLOZE_TEMPLATE_FILE = {
+  ...BASIC_TEMPLATE_FILE,
+  storageKey: 'n2a-cloze',
+};
+const INPUT_TEMPLATE_FILE = {
+  ...BASIC_TEMPLATE_FILE,
+  storageKey: 'n2a-input',
+};
 
-const CLOZE_TEMPLATE_FILE = { ...BASIC_TEMPLATE_FILE, storageKey: 'n2a-cloze' };
 test.each([
   ['basic template', 'n2a-basic'],
   ['cloze template', 'n2a-cloze'],
   ['input template', 'n2a-input'],
 ])('%s', (_, storageKey) => {
   expect(
-    getCustomTemplate(storageKey, [BASIC_TEMPLATE_FILE, CLOZE_TEMPLATE_FILE])
-      ?.storageKey
+    getCustomTemplate(storageKey, [
+      BASIC_TEMPLATE_FILE,
+      CLOZE_TEMPLATE_FILE,
+      INPUT_TEMPLATE_FILE,
+    ])?.storageKey
   ).toBe(storageKey);
 });
