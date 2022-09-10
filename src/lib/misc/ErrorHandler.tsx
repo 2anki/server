60d928cc290f944dfe52a2967b41baffcd9dbfa9
Alexander Alemayhu <alexander@alemayhu.com>
2022-09-10 17:38:02 +0200

    feat: encourage users to share their page

--- src/lib/misc/ErrorHandler.tsx
+++ src/lib/misc/ErrorHandler.tsx
@@ -2,11 +2,14 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import * as Sentry from '@sentry/node';
 import express from 'express';
 
+const ADMIN_EMAIL = 'alexander@alemayhu.com';
 export const NO_PACKAGE_ERROR = new Error(
   renderToStaticMarkup(
     <div className="info">
       Could not create a deck using your file and rules. Please review your{' '}
-      <a href="/upload?view=template">settings</a>
+      <a href="/upload?view=template">settings</a> or send an email to{' '}
+      <a href={`mailto:${ADMIN_EMAIL}`}> {ADMIN_EMAIL}</a> with the page you
+      uploaded.
     </div>
   )
 );
