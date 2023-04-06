8dc388e0557dc28b897a62754fe06d56a86d4e3b
Alexander Alemayhu <alexander@alemayhu.com>
2023-04-06 06:35:15 +0200

    refactor: move cleanup function into own file
    Also reduce max keys to 10K. 110K is using up too much memory for
    cleanup:
    
    	<--- JS stacktrace --->
    
    	FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
    	 1: 0xb00e10 node::Abort() [node]
    	 2: 0xa1823b node::FatalError(char const*, char const*) [node]
    	 3: 0xcee09e v8::Utils::ReportOOMFailure(v8::internal::Isolate*, char const*, bool) [node]
    	 4: 0xcee417 v8::internal::V8::FatalProcessOutOfMemory(v8::internal::Isolate*, char const*, bool) [node]
    	 5: 0xea65d5  [node]
    	 6: 0xea70b6  [node]
    	 7: 0xeb4fee  [node]
    	 8: 0xeb5a30 v8::internal::Heap::CollectGarbage(v8::internal::AllocationSpace, v8::internal::GarbageCollectionReason, v8::GCCallbackFlags) [node]
    	 9: 0xeb89ae v8::internal::Heap::AllocateRawWithRetryOrFailSlowPath(int, v8::internal::AllocationType, v8::internal::AllocationOrigin, v8::internal::AllocationAlignment) [node]
    	10: 0xe79dda v8::internal::Factory::NewFillerObject(int, bool, v8::internal::AllocationType, v8::internal::AllocationOrigin) [node]
    	11: 0x11f33d6 v8::internal::Runtime_AllocateInYoungGeneration(int, unsigned long*, v8::internal::Isolate*) [node]
    	12: 0x15e7cf9  [node]
    	(node:8) NOTE: We are formalizing our plans to enter AWS SDK for JavaScript (v2) into maintenance mode in 2023.
    
    	Please migrate your code to use AWS SDK for JavaScript (v3).
    	For more information, check the migration guide at https://a.co/7PzMCcy
    	(Use `node --trace-warnings ...` to show where the warning was created)
    	getting max 110000 keys
    	getting max 110000 keys
    
    	<--- Last few GCs --->
    
    	[8:0x59d5210]  1599036 ms: Scavenge 1940.0 (2079.4) -> 1926.2 (2079.4) MB, 12.4 / 0.0 ms  (average mu = 0.231, current mu = 0.167) allocation failure
    	[8:0x59d5210]  1599098 ms: Scavenge 1941.1 (2079.4) -> 1927.3 (2080.4) MB, 11.8 / 0.0 ms  (average mu = 0.231, current mu = 0.167) allocation failure
    	[8:0x59d5210]  1601261 ms: Mark-sweep 1943.2 (2081.3) -> 1924.4 (2079.5) MB, 2030.2 / 0.1 ms  (average mu = 0.222, current mu = 0.212) allocation failure scavenge might not succeed
    	# Please enter the commit message for your changes. Lines starting

--- src/lib/storage/jobs/ScheduleCleanup.ts
+++ src/lib/storage/jobs/ScheduleCleanup.ts
@@ -1,16 +1,8 @@
 import { Knex } from 'knex';
 
-import deleteOldUploads, { MS_21 } from './helpers/deleteOldUploads';
-import deleteOldFiles from './helpers/deleteOldFiles';
+import { MS_21 } from './helpers/deleteOldUploads';
+import { runCleanup } from './helpers/runCleanup';
 
 export const ScheduleCleanup = (db: Knex) => {
-  setInterval(async () => {
-    console.time('running cleanup');
-    deleteOldFiles();
-    await deleteOldUploads(db);
-    db.raw(
-      "DELETE FROM jobs WHERE created_at < NOW() - INTERVAL '14 days' AND status = 'failed'"
-    );
-    console.timeEnd('running cleanup');
-  }, MS_21);
+  setInterval(() => runCleanup(db), MS_21);
 };
---
+++ src/lib/storage/jobs/helpers/runCleanup.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+import deleteOldFiles from './deleteOldFiles';
+import deleteOldUploads from './deleteOldUploads';
+
+export const runCleanup = async (db: Knex) => {
+  console.time('running cleanup');
+  deleteOldFiles();
+  await deleteOldUploads(db);
+  db.raw(
+    "DELETE FROM jobs WHERE created_at < NOW() - INTERVAL '14 days' AND status = 'failed'"
+  );
+  console.timeEnd('running cleanup');
+};
