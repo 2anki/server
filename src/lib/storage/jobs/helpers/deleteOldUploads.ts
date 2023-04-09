fbbb5c299fafbfe5c1a12c395a8a27b96f5f64df
Alexander Alemayhu <alexander@alemayhu.com>
2023-04-09 08:34:06 +0200

    Revert "fix: delete outdated uploads asynchronous"
    This reverts commit 95387387e4c46c1e1efdd00ee19059967179b63d.

--- src/lib/storage/jobs/helpers/deleteOldUploads.ts
+++ src/lib/storage/jobs/helpers/deleteOldUploads.ts
@@ -5,7 +5,6 @@ import Users from '../../../../schemas/public/Users';
 import { TIME_21_MINUTES_AS_SECONDS } from '../../../constants';
 import StorageHandler from '../../StorageHandler';
 import { Upload } from '../../types';
-import { sendError } from '../../../error/sendError';
 
 export const MS_21 = TIME_21_MINUTES_AS_SECONDS * 1000;
 const MAX_KEYS = 100_000;
@@ -16,61 +15,6 @@ const getFreeUsers = (db: Knex): Promise<Users[]> =>
 const getUploadsForUser = (user: Users, db: Knex): Promise<Upload> =>
   db('uploads').where('owner', user.id).returning(['key']);
 
-const purgeUploads = async (
-  db: Knex,
-  storage: StorageHandler,
-  uploads: Upload[]
-) => {
-  console.debug('purging anon uploads');
-  for (const upload of uploads.flat()) {
-    console.debug('delete', upload.key);
-    await storage.deleteWith(upload.key);
-    await db('uploads').delete().where('key', upload.key);
-  }
-};
-
-const purgeBucket = async (db: Knex, storage: StorageHandler) => {
-  console.debug('purging bucket uploads');
-  const query = await db.raw(`
-    SELECT up.key FROM users u JOIN uploads up ON u.id = up.owner WHERE u.patreon = true;
-  `);
-  const anonUploads = query.rows as Uploads[];
-  const { s3 } = storage;
-
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      let hasMore = true;
-      while (hasMore) {
-        const objects = await s3
-          .listObjects({
-            Bucket: StorageHandler.DefaultBucketName(),
-            MaxKeys: MAX_KEYS,
-          })
-          .promise();
-        console.debug('found', objects.Contents?.length, 'uploads');
-        if (objects.Contents) {
-          const nonPatreonFiles =
-            objects.Contents?.filter(
-              (f) => f.Key && anonUploads.find((up) => up.key !== f.Key)
-            ) || [];
-          for (const no of nonPatreonFiles) {
-            if (no.Key) {
-              storage.deleteWith(no.Key);
-            }
-          }
-        }
-        hasMore = Boolean(objects.IsTruncated);
-      }
-    } catch (err) {
-      if (err) {
-        sendError(err);
-        return reject(err);
-      }
-    }
-    resolve();
-  });
-};
-
 export default async function deleteOldUploads(db: Knex) {
   const storage = new StorageHandler();
   const users = await getFreeUsers(db);
@@ -78,7 +22,24 @@ export default async function deleteOldUploads(db: Knex) {
     users.map((user) => getUploadsForUser(user, db))
   );
 
-  purgeUploads(db, storage, uploads);
-  await purgeBucket(db, storage);
-  console.debug('done cleanup');
+  for (const upload of uploads.flat()) {
+    console.debug('delete', upload.key);
+    await storage.deleteWith(upload.key);
+    await db('uploads').delete().where('key', upload.key);
+  }
+
+  const files = await storage.getContents(MAX_KEYS);
+  const query = await db.raw(`
+    SELECT up.key FROM users u JOIN uploads up ON u.id = up.owner WHERE u.patreon = true;
+  `);
+
+  const anonUploads = query.rows as Uploads[];
+  const nonPatreonFiles =
+    files?.filter((f) => f.Key && anonUploads.find((up) => up.key !== f.Key)) ||
+    [];
+  for (const no of nonPatreonFiles) {
+    if (no.Key) {
+      storage.deleteWith(no.Key);
+    }
+  }
 }
