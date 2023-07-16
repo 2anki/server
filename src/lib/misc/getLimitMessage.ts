74da5f042bcda015726b734544e7f45078f5c59d
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-16 07:40:07 +0200

    refactor: Update getLimitMessage to use JSX and include additional options

--- src/lib/misc/getLimitMessage.ts
+++
@@ -1,4 +0,0 @@
-export const getLimitMessage =
-  () => `Your request has hit the limit, there is a max to prevent abuse.
-        <a href="https://alemayhu.com/patreon">Become a patron</a> to remove
-      the limit.`;
