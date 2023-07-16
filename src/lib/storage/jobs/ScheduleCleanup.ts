fc0f1bd0c5830d5b1091a67fe06c525632a912b5
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-16 16:36:45 +0200

    Revert "fix(storage): run cleanup every hour" (#1227)
    Reverts 2anki/2anki.net#1225

--- src/lib/storage/jobs/ScheduleCleanup.ts
+++ src/lib/storage/jobs/ScheduleCleanup.ts
@@ -1,10 +1,8 @@
 import { Knex } from 'knex';
 
-import { ONE_HOUR } from '../../constants';
+import { MS_21 } from './helpers/deleteOldUploads';
 import { runCleanup } from './helpers/runCleanup';
 
-const EVERY_HOUR_MS = ONE_HOUR * 1000;
-
 export const ScheduleCleanup = (db: Knex) => {
-  setInterval(() => runCleanup(db), EVERY_HOUR_MS);
+  setInterval(() => runCleanup(db), MS_21);
 };
