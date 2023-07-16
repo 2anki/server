870527a771cd3b391d130a72263b28f9cc7e8f50
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-16 16:36:31 +0200

    Revert "fix(storage): run cleanup every hour"
    This reverts commit 84702bf404d7527a91ba4065badaf4f41b0d91a6.

--- src/lib/storage/jobs/helpers/deleteOldFiles.ts
+++ src/lib/storage/jobs/helpers/deleteOldFiles.ts
@@ -2,9 +2,7 @@ import os from 'os';
 import path from 'path';
 
 import findRemoveSync from 'find-remove';
-import { ONE_HOUR } from '../../../constants';
-
-const ONE_HOUR_OLD = ONE_HOUR * 1000;
+import { TIME_21_MINUTES_AS_SECONDS } from '../../../constants';
 
 export default function deleteOldFiles() {
   const locations = ['workspaces', 'uploads'];
@@ -12,7 +10,7 @@ export default function deleteOldFiles() {
     console.time(`finding & removing ${loc} files older than 21 minutes`);
     findRemoveSync(path.join(os.tmpdir(), loc), {
       files: '*.*',
-      age: { seconds: ONE_HOUR_OLD },
+      age: { seconds: TIME_21_MINUTES_AS_SECONDS },
     });
     console.timeEnd(`finding & removing ${loc} files older than 21 minutes`);
   }
--- src/lib/storage/jobs/helpers/deleteOldUploads.ts
+++ src/lib/storage/jobs/helpers/deleteOldUploads.ts
@@ -1,8 +1,10 @@
 import { Knex } from 'knex';
 import Uploads from '../../../../data_layer/public/Uploads';
 
+import { TIME_21_MINUTES_AS_SECONDS } from '../../../constants';
 import StorageHandler from '../../StorageHandler';
 
+export const MS_21 = TIME_21_MINUTES_AS_SECONDS * 1000;
 const MAX_KEYS = 100_000;
 
 const deleteNonSubScriberUploads = async (
