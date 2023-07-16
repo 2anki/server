c1b4f5bd71e43b40ce5695bd8594743cd3265a9c
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-16 06:53:33 +0200

    fix/unknown file (#1224)
    We are seeing two new errors in Sentry
    
    > URI malformed
    
    > Error Unknown file [redacted]

--- src/lib/parser/DeckParser.ts
+++ src/lib/parser/DeckParser.ts
@@ -40,14 +40,11 @@ export class DeckParser {
     this.files = files || [];
     this.firstDeckName = name;
     this.globalTags = null;
-    const firstFile = this.files.find((file) => {
-      try {
-        return file.name === global.decodeURIComponent(name);
-      } catch (error) {
-        sendError(error);
-        return file.name === name;
-      }
-    });
+
+    const firstFile = this.files.find((file) =>
+      this.isFileNameEqual(file, name)
+    );
+
     const contents = firstFile?.contents;
     if (contents) {
       this.payload = this.handleHTML(
@@ -59,6 +56,38 @@ export class DeckParser {
     } else {
       throw new Error(`Error Unknown file ${name}`);
     }
+  }
+
+  /**
+   * Check if the file name is equal to the name
+   * This can be used to find the contents of a file in the payload.
+   * Due to encoding issues, we need to check both the encoded and decoded names
+   *
+   * @param file uploaded file from user
+   * @param name name of the file
+   * @returns true if the file name is equal to the name
+   */
+  isFileNameEqual(file: File, name: string) {
+    try {
+      // For backwards compatibility, we need to support the old way of parsing
+      const decodedName = global.decodeURIComponent(name);
+      if (file.name === decodedName) {
+        return true;
+      }
+    } catch (error) {
+      console.debug('Failed to decode name');
+    }
+
+    try {
+      const decodedFilename = global.decodeURIComponent(file.name);
+      const decodedName = global.decodeURIComponent(name);
+
+      return decodedFilename === decodedName;
+    } catch (error) {
+      console.debug('Failed to decode names');
+    }
+
+    return file.name === name;
   }
 
   findNextPage(href: string | undefined): string | Uint8Array | undefined {
