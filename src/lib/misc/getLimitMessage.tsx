268e29f6006c8eb561a8dfc9b4c8e266c2f6e540
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-16 16:54:10 +0200

    refactor: Update getLimitMessage to use JSX and include additional options (#1226)

---
+++ src/lib/misc/getLimitMessage.tsx
@@ -0,0 +1,21 @@
+import { renderToStaticMarkup } from 'react-dom/server';
+
+export const getLimitMessage = () =>
+  renderToStaticMarkup(
+    <>
+      <strong>
+        Your request has hit the limit, there is a max to prevent abuse. You
+        have two options:
+      </strong>
+      <ul>
+        <li>
+          Split your request into multiple smaller ones (i.e.) make your upload
+          size smaller.
+        </li>
+        <li>
+          <a href="https://alemayhu.com/patreon">Become a patron</a> to remove
+          the limit.
+        </li>
+      </ul>
+    </>
+  );
