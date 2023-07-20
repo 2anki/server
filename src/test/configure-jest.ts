df4980d3334f5b368bb14827f883d0747631f236
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-20 20:44:11 +0200

    test: silence more of the various console calls

--- src/test/configure-jest.ts
+++ src/test/configure-jest.ts
@@ -4,4 +4,8 @@ import os from 'os';
 export const setupTests = () => {
   process.env.WORKSPACE_BASE = path.join(os.tmpdir(), 'workspaces');
   jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'time').mockImplementation(() => {});
+  jest.spyOn(console, 'debug').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
 };
