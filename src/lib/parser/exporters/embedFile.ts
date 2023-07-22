3f552ef2a47e511a9376b4449f70bc6e1aa37c9c
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-22 16:19:20 +0200

    fix(embedFile): improve the file retrieval from the payload
    This will fix several missing file paths.
    
    Close: https://github.com/2anki/2anki.net/issues/1133
    Related-to: https://github.com/2anki/2anki.net/issues/901

--- src/lib/parser/exporters/embedFile.ts
+++ src/lib/parser/exporters/embedFile.ts
@@ -4,34 +4,57 @@ import { SuffixFrom } from '../../misc/file';
 import getUniqueFileName from '../../misc/getUniqueFileName';
 import CustomExporter from './CustomExporter';
 
+const getFile = (
+  exporter: CustomExporter,
+  files: File[],
+  filePath: string
+): File | undefined => {
+  const asRootFile = files.find((f) => f.name === filePath);
+  if (asRootFile) {
+    return asRootFile;
+  }
+  const parent = exporter.firstDeckName.replace(/.html /, '/');
+  const asChildFile = files.find((f) => f.name === `${parent}/${filePath}`);
+  if (asChildFile) {
+    return asChildFile;
+  }
+
+  /*
+   * Could not find file, try to find it by ending.
+   * This happens in deeply nested directories.
+   * Example: using a huge database
+   */
+  const normalized = filePath.replace(/\.\.\//g, '');
+  const usingSuffix = files.find(
+    (f) => f.name.endsWith(filePath) || f.name.endsWith(normalized)
+  );
+  if (usingSuffix) {
+    return usingSuffix;
+  }
+
+  return undefined;
+};
+
 export const embedFile = (
   exporter: CustomExporter,
   files: File[],
   filePath: string
 ): string | null => {
   const suffix = SuffixFrom(filePath);
-  let file = files.find((f) => f.name === filePath);
-  if (!file) {
-    const lookup = `${exporter.firstDeckName}/${filePath}`.replace(
-      /\.\.\//g,
-      ''
-    );
-    file = files.find((f) => {
-      if (f.name === lookup || f.name.endsWith(filePath)) {
-        return f;
-      }
-    });
-    if (!file) {
-      Bugsnag.notify(
-        `Missing relative path to ${filePath} used ${exporter.firstDeckName}`
-      );
-      return null;
+  const file = getFile(exporter, files, filePath);
+
+  if (file) {
+    const newName = getUniqueFileName(filePath) + suffix;
+    const contents = file.contents as string;
+    if (contents) {
+      exporter.addMedia(newName, contents);
     }
+    return newName;
+  } else {
+    Bugsnag.notify(
+      `Missing relative path to ${filePath} used ${exporter.firstDeckName}`
+    );
   }
-  const newName = getUniqueFileName(filePath) + suffix;
-  const contents = file.contents as string;
-  if (contents) {
-    exporter.addMedia(newName, contents);
-  }
-  return newName;
+
+  return null;
 };
