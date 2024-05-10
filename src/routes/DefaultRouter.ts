eb97843016890a673f3e4ecb0cf6f5d9bd0a8cbe
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-10 17:20:17 +0200

    fix: add email to limit message

--- src/routes/DefaultRouter.ts
+++ src/routes/DefaultRouter.ts
@@ -6,6 +6,7 @@ const DefaultRouter = () => {
   const controller = new IndexController();
   const router = express.Router();
 
+  router.get('/index.html', (req, res) => controller.getIndex(req, res));
   router.get('/search*', RequireAuthentication, controller.getIndex);
   router.get('*', (req, res) => controller.getIndex(req, res));
 
