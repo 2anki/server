f5920ebfe1770e386e2bbb8671cebd295a52ddcb
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-17 13:22:38 +0200

    fix: skip the empty uploads (#1519)
    No idea why someone would upload empty files but it happens.

---
+++ src/lib/misc/isEmptyPayload.tsx
@@ -0,0 +1,9 @@
+import { UploadedFile } from '../storage/types';
+
+export const isEmptyPayload = (files: UploadedFile[] | undefined) => {
+  if (!files || !Array.isArray(files) || files.length === 0) {
+    return true;
+  }
+  const totalBytes = files.reduce((acc, file) => acc + file.size, 0);
+  return totalBytes > 0;
+};
