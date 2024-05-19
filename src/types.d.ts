270b72e76b3469dc8b55222b894f516230b5ff11
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-19 09:23:03 +0200

    refactor: remove unused types.d.ts

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-declare module './private/features/notion/NotionRouter' {
-  export const NotionRouter: () => express.Router;
-}
-
-declare module './private/features/favorites/FavoriteRouter' {
-  export const FavoriteRouter: () => express.Router;
-}
