7b0d8f39e67b0217932277de5e852cffa8ab7ba0
Alexander Alemayhu <alexander@alemayhu.com>
2024-08-08 19:20:34 +0200

    feat: add endpoint for contacting support (#1570)
    This is a work in progress.

--- src/routes/DefaultRouter.ts
+++ src/routes/DefaultRouter.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import multer from 'multer';
+
 import RequireAuthentication from './middleware/RequireAuthentication';
 import IndexController from '../controllers/IndexController/IndexController';
+
+const upload = multer({
+  limits: { fileSize: 25 * 1024 * 1024 },
+});
 
 const DefaultRouter = () => {
   const controller = new IndexController();
@@ -9,6 +15,10 @@ const DefaultRouter = () => {
   router.get('/index.html', (req, res) => controller.getIndex(req, res));
   router.get('/search*', RequireAuthentication, controller.getIndex);
   router.get('*', (req, res) => controller.getIndex(req, res));
+
+  router.post('/api/contact-us', upload.array('attachments'), (req, res) =>
+    controller.contactUs(req, res)
+  );
 
   return router;
 };
