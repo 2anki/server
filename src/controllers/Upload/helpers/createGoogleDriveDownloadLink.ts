734cd0637566f613b7c34dae1c2d1260677fcc1f
Alexander Alemayhu <alexander@alemayhu.com>
2024-08-18 14:45:14 +0200

    docs: mention why we generate this link

--- src/controllers/Upload/helpers/createGoogleDriveDownloadLink.ts
+++ src/controllers/Upload/helpers/createGoogleDriveDownloadLink.ts
@@ -1,5 +1,10 @@
 import { GoogleDriveFile } from '../../../data_layer/GoogleDriveRepository';
 
+/**
+ * Create a download link for a Google Drive file. The default URL is just a preview link and request user interaction
+ * to download the file. This link will directly download the file.
+ * @param file
+ */
 export function createGoogleDriveDownloadLink(file: GoogleDriveFile) {
   return 'https://www.googleapis.com/drive/v3/files/' + file.id + '?alt=media';
 }
