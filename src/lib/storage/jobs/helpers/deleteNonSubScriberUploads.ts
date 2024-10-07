7f6138967264a1b8515a98ba32c88a69a441f988
Alexander Alemayhu <alexander@alemayhu.com>
2024-10-07 21:17:02 +0200

    refactor: split deletion functions into own files

---
+++ src/lib/storage/jobs/helpers/deleteNonSubScriberUploads.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+import StorageHandler from '../../StorageHandler';
+import Uploads from '../../../../data_layer/public/Uploads';
+
+export const deleteNonSubScriberUploads = async (
+  db: Knex,
+  storage: StorageHandler
+) => {
+  const query = await db.raw(`
+    SELECT up.key 
+    FROM users u 
+    JOIN uploads up ON u.id = up.owner 
+    LEFT JOIN subscriptions s ON u.email = s.email OR u.email = s.linked_email
+    WHERE u.patreon = false AND (s.active IS NULL OR s.active = false);
+  `);
+  const nonSubScriberUploads: Uploads[] | undefined = query.rows;
+  if (!nonSubScriberUploads) {
+    return;
+  }
+
+  for (const upload of nonSubScriberUploads.flat()) {
+    await storage.delete(upload.key);
+    await db('uploads').delete().where('key', upload.key);
+  }
+};
