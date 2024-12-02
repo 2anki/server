63bd2d9c8a11829ab21e7827fce7f19a2955d142
Alexander Alemayhu <alexander@alemayhu.com>
2024-12-02 11:01:58 +0100

    chore: treat PDF limit as regular limit
    This prevents the default error reporting from triggering.

--- src/lib/misc/isLimitError.ts
+++ src/lib/misc/isLimitError.ts
@@ -1,7 +1,10 @@
+import { PDF_EXCEEDS_MAX_PAGE_LIMIT } from '../parser/pdf/convertPDFToImages';
+
 const LIMIT_MESSAGES = [
   'File too large',
   'You can only add 100 cards',
   'Your request has hit the limit',
+  PDF_EXCEEDS_MAX_PAGE_LIMIT,
 ];
 
 export const isLimitError = (error?: Error) => {
