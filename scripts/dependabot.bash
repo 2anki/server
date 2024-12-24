5b5d4853962a6fa5ff2c1c183ff474c9e2c8d80a
Alexander Alemayhu <alexander@alemayhu.com>
2024-12-24 05:42:24 +0100

    scripts: skip pushing

--- scripts/dependabot.bash
+++ scripts/dependabot.bash
@@ -6,6 +6,5 @@ gh pr list --search "author:app/dependabot is:open" --json number,headRefName --
   echo "Merging PR #$pr_number from branch $branch_name into $current_branch"
   git fetch origin $branch_name
   git merge --no-ff --no-verify origin/$branch_name
-  git push origin $current_branch
   gh pr close $pr_number --comment "Merged successfully into $current_branch."
 done
