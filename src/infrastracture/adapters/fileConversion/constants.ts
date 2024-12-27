b645865a2a0469d4bfe12db6922329e73a4bb112
Alexander Alemayhu <alexander@alemayhu.com>
2024-12-27 05:03:50 +0100

    refactor: reduce vertex duplication
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/infrastracture/adapters/fileConversion/constants.ts
+++ src/infrastracture/adapters/fileConversion/constants.ts
@@ -1,12 +1,16 @@
-import { HarmBlockThreshold, HarmCategory } from '@google-cloud/vertexai';
+import {
+  HarmBlockThreshold,
+  HarmCategory,
+  SafetySetting,
+} from '@google-cloud/vertexai';
 
-export const SAFETY_SETTINGS = [
+export const SAFETY_SETTINGS: SafetySetting[] = [
   {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
     threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
     threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
@@ -14,7 +18,18 @@ export const SAFETY_SETTINGS = [
     threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
     threshold: HarmBlockThreshold.BLOCK_NONE,
   },
 ];
+
+export const VERTEX_AI_CONFIG = {
+  project: 'notion-to-anki',
+  location: 'europe-west3',
+  model: 'gemini-1.5-pro-002',
+  generationConfig: {
+    maxOutputTokens: 8192,
+    temperature: 1,
+    topP: 0.95,
+  },
+};
--- src/infrastracture/adapters/fileConversion/convertPDFToHTML.ts
+++ src/infrastracture/adapters/fileConversion/convertPDFToHTML.ts
@@ -1,25 +1,9 @@
 import path from 'path';
 import fs from 'fs';
+import { GenerateContentRequest } from '@google-cloud/vertexai';
+import { generateContent } from './contentGenerationUtils';
 
-import { GenerateContentRequest, VertexAI } from '@google-cloud/vertexai';
-import { SAFETY_SETTINGS } from './constants';
-
-export const convertPDFToHTML = async (pdf: string): Promise<string> => {
-  const vertexAI = new VertexAI({
-    project: 'notion-to-anki',
-    location: 'europe-west3',
-  });
-  const model = 'gemini-1.5-pro-002';
-  const generativeModel = vertexAI.preview.getGenerativeModel({
-    model: model,
-    generationConfig: {
-      maxOutputTokens: 8192,
-      temperature: 1,
-      topP: 0.95,
-    },
-    safetySettings: SAFETY_SETTINGS,
-  });
-
+export const convertPDFToHTML = (pdf: string): Promise<string> => {
   const document1 = {
     inlineData: {
       mimeType: 'application/pdf',
@@ -43,27 +27,5 @@ export const convertPDFToHTML = async (pdf: string): Promise<string> => {
     contents: [{ role: 'user', parts: [document1, text1] }],
   };
 
-  let htmlContent = '';
-  try {
-    const streamingResp = await generativeModel.generateContentStream(req);
-    for await (const item of streamingResp.stream) {
-      if (
-        item.candidates &&
-        item.candidates[0].content &&
-        item.candidates[0].content.parts
-      ) {
-        htmlContent += item.candidates[0].content.parts
-          .map((part) => part.text)
-          .join('');
-      }
-    }
-  } catch (error) {
-    console.error('Error generating content stream:', error);
-
-    // const workSpace = process.cwd();
-    // const outputPath = path.join(workSpace, 'output.html');
-    // fs.writeFileSync(outputPath, htmlContent);
-    // console.log(outputPath);
-  }
-  return htmlContent;
+  return generateContent(req);
 };
