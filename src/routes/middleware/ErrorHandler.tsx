fe305c616cc1a13523fe9c0a107eebca9c4d5d32
Alexander Alemayhu <alexander@alemayhu.com>
2025-05-01 11:10:15 +0200

    chore: send email errors
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/routes/middleware/ErrorHandler.tsx
+++ src/routes/middleware/ErrorHandler.tsx
@@ -1,10 +1,41 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { UploadedFile } from '../../lib/storage/types';
 import { isLimitError } from '../../lib/misc/isLimitError';
 import { isEmptyPayload } from '../../lib/misc/isEmptyPayload';
 import { perserveFilesForDebugging } from '../../lib/debug/perserveFilesForDebugging';
 
-export default function ErrorHandler(
+const transporter = nodemailer.createTransport({
+  sendmail: true,
+  newline: 'unix',
+  path: '/usr/sbin/sendmail',
+});
+
+async function sendErrorEmail(error: Error, req: express.Request) {
+  if (process.env.NODE_ENV !== 'production') return;
+
+  const message = {
+    from: process.env.ERROR_SENDER_EMAIL || 'noreply@zoe.2anki.net',
+    to: process.env.ERROR_RECEIVER_EMAIL || 'alexander@alemayhu.com',
+    subject: `[ERROR] [2anki.net] ${error.name}: ${error.message}`,
+    text: `
+Error: ${error.stack}
+
+Request path: ${req.path}
+Method: ${req.method}
+Query: ${JSON.stringify(req.query)}
+Body: ${JSON.stringify(req.body)}
+`,
+  };
+
+  try {
+    await transporter.sendMail(message);
+  } catch (emailErr) {
+    console.error('Failed to send error email:', emailErr);
+  }
+}
+
+export default async function ErrorHandler(
   res: express.Response,
   req: express.Request,
   err: Error
@@ -16,8 +47,9 @@ export default function ErrorHandler(
     console.info('Send error');
     console.error(err);
     if (!isEmptyPayload(uploadedFiles)) {
-      perserveFilesForDebugging(req.files as UploadedFile[], err);
+      perserveFilesForDebugging(uploadedFiles, err);
     }
+    await sendErrorEmail(err, req);
   }
 
   res.set('Content-Type', 'text/plain');
