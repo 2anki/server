8185ba6efcb08eaf0cc2f20f9a1167f04cf976dd
Alexander Alemayhu <alexander@alemayhu.com>
2025-05-24 20:24:37 +0200

    feat: add custom format link
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/lib/error/constants.tsx
+++ src/lib/error/constants.tsx
@@ -6,13 +6,12 @@ export const NO_PACKAGE_ERROR = new Error(
       <div className="info">
         Could not create a deck using your file(s) and rules. Make sure to at
         least create on valid toggle or verify your{' '}
-        <a href="/upload?view=template">settings</a>? Example:
-      </div>
-      <div className="card" style={{ width: '50%', padding: '1rem' }}>
-        <details>
-          <summary>This the front</summary>
-          This is the back
-        </details>
+        <a href="/upload?view=template">settings</a>.<br /> Alternatively, you
+        can try to convert your file(s) using{' '}
+        <a href={'https://custom-format.2anki.net/custom-format'}>
+          2anki custom format
+        </a>
+        .
       </div>
     </>
   )
