68d121f08e0356fb110ac6633569088f3c2bd7bc
Alexander Alemayhu <alexander@alemayhu.com>
2026-04-05 14:01:39 +0200

    fix: deduplicate rows before adding unique(owner,name) constraint

--- migrations/20260404000001_public_templates_unique_owner_name.js
+++ migrations/20260404000001_public_templates_unique_owner_name.js
@@ -1,4 +1,13 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
+  await knex.raw(`
+    DELETE FROM public_templates
+    WHERE id NOT IN (
+      SELECT MIN(id)
+      FROM public_templates
+      GROUP BY owner, name
+    )
+  `);
+
   return knex.schema.table('public_templates', function (table) {
     table.unique(['owner', 'name']);
   });
