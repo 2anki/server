463c644893e5b493cf13414f3cacb3eb6cec2e81
Alexander Alemayhu <alexander@alemayhu.com>
2026-04-12 19:31:31 +0200

    fix: omit base64 strings from deck name
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/services/NotionService/helpers/renderIcon.tsx
+++ src/services/NotionService/helpers/renderIcon.tsx
@@ -5,15 +5,17 @@ export default async function renderIcon(icon?: string) {
   if (!icon) {
     return null;
   }
-  if (icon.startsWith('http')) {
-    let validIcon = true;
-    await axios.get(icon).catch(function (error) {
-      if (error.response && error.response.status === 404) {
-        validIcon = false;
+  if (icon.startsWith('http') || icon.startsWith('data:image')) {
+    if (icon.startsWith('http')) {
+      let validIcon = true;
+      await axios.get(icon).catch(function (error) {
+        if (error.response && error.response.status === 404) {
+          validIcon = false;
+        }
+      });
+      if (!validIcon) {
+        return null;
       }
-    });
-    if (!validIcon) {
-      return null;
     }
     return renderToStaticMarkup(<img src={icon} width="32" />);
   }
